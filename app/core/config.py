11f1d1c1924a88d9a05ef45786e33936790a6957
roger-king <rogr.king@gmail.com>
2021-12-04 14:27:07 -0500

    feat: twitter and channels environment variables

--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,4 @@
-import secrets
+import os
 
 from pydantic import BaseSettings
 
@@ -6,6 +6,12 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     API_STR: str = "/api"
     PROJECT_NAME: str = "FastAPI"
+    CHANNELS_TO_WATCH: list[str] = [] if len(os.getenv(
+        "SLACK_CHANNELS", "")) == 0 else os.getenv("SLACK_CHANNELS", "").split(",")
+    TWITTER_CONSUMER_KEY: str = os.getenv("TWITTER_API_KEY")
+    TWITTER_CONSUMER_SECRET: str = os.getenv("TWITTER_API_SECRET_KEY")
+    TWITTER_ACCESS_TOKEN: str = os.getenv("TWITTER_ACCESS_TOKEN")
+    TWITTER_ACCESS_SECRET: str = os.getenv("TWITTER_ACCESS_SECRET")
 
     class Config:
         env_file = ".env"
