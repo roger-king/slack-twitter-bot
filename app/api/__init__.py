4a49df1b5327577592d4378d8dcb6544acadec2d
roger-king <rogr.king@gmail.com>
2021-12-04 14:26:53 -0500

    feat: slack events endpoint to process events

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from app.api import status
+from app.api import slack
 
 api_router = APIRouter()
 api_router.include_router(
     status.router, prefix="/status", tags=["status"])
+api_router.include_router(slack.router, prefix="/slack", tags=["slack"])
