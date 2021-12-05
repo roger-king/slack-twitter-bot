90ce74b70b0caf8e05c4bb53accb85fdafe07cf4
Roger King <10982875+roger-king@users.noreply.github.com>
2021-12-05 17:07:13 -0500

    fix: dynamically set reload and port

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
-from app.main import app
+import os
 import uvicorn
 
 if __name__ == "__main__":
     # TODO: make the reload dynamic
-    uvicorn.run("app.main:app", host="0.0.0.0", port=9000, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=int(
+        os.getenv("PORT", 9000)), reload=True if os.getenv("SERVER_ENV", "local") != 'local' else False)
